1ccdc73b68ca909f50cc9b6f6124b06acaab19f3
ru-lakshya <rishihood.lakshya@gmail.com>
2026-05-03 18:50:03 +0530

    base config

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
-import flask
+import os
+import json
+from datetime import datetime
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from supabase import create_client
+from dotenv import load_dotenv
 
-app = flask.Flask(__name__)
+load_dotenv()
 
-@app.route("/")
-def index():
-    return "Hello World from backend!!"
+app = Flask(__name__)
+CORS(app)
 
-app.run(debug=True)
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+
+supabase_client = create_client(supabase_url, supabase_key)
+
+# base app
+
+@app.route("/", methods=["GET"])
+def base_app():
+    return jsonify({"status": "App is running..."}), 200
+
+# get product
+
+@app.route("/products", methods=["GET"])
+def get_products():
+    try:
+        res = supabase_client.table("products").select("*").execute()
+        return jsonify({"products": res.data}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# create product
+
+# delete product
+
+# update product
+
+# checkout
+
+
+if __name__ == "__main__":
+    app.run(port=5005, debug=True)
